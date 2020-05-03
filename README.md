1c7b1820afa7c36f6a87ef39e35715c56b79f164
Li Yi <denverdino@gmail.com>
2020-05-03 11:11:36 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Scan images in Alibaba Cloud Container Registry Service
+Use this GitHub Action to scan images in [Alibaba Cloud Container Registry](https://www.aliyun.com/product/acr). 
+
+```yaml
+- uses: denverdino/acr-scan@v0.1.0
+  with:
+    region-id: '<region id>' # example: cn-hangzhou
+    access-key-id: '<access key id>'
+    access-key-secret: '<access key secret>'
+    repository: '<repository>' # example: test/nginx
+    tag: '<tag>' # example: latest
+```
+
+Or
+
+```yaml
+- uses: denverdino/acr-scan@v0.1.0
+  with:
+    region-id: '<region id>' # example: cn-hangzhou
+    access-key-id: '<access key id>'
+    access-key-secret: '<access key secret>'
+    instance-id: '<registry instance id>'
+    repository: '<repository>' # example: test/nginx
+    tag: '<tag>' # example: latest
+```
+
+Refer to the action metadata file for details about all the inputs: [action.yml](https://github.com/denverdino/acr-scan/blob/master/action.yml)
+
+
+### Prerequisite
+Get the access-key-id and access-key-secret of your container registry 
+
+And add access-key-id and access-key-secret as [secret](https://developer.github.com/actions/managing-workflows/storing-secrets/) in the GitHub repository.
---
+++ src/scan.js
@@ -0,0 +1,92 @@
+const core = require('@actions/core');
+const io = require('@actions/io');
+const { issueCommand } = require('@actions/core/lib/command');
+
+const path = require('path');
+const fs = require('fs');
+const ROAClient = require('@alicloud/pop-core').ROAClient;
+const RPCClient = require('@alicloud/pop-core').RPCClient;
+
+
+function getAPIEndpoint(regionId) {
+    return `https://cr.${regionId}.aliyuncs.com`
+}
+
+function getRegistryEndpoint(regionId) {
+    return `https://registry.${regionId}.aliyuncs.com`
+}
+
+async function run() {
+    let accessKeyId = core.getInput('access-key-id', { required: false });
+    let accessKeySecret = core.getInput('access-key-secret', { required: false });
+    let regionId = core.getInput('region-id', { required: false });
+    let instanceId = core.getInput('instance-id', { required: false });
+    let repository = core.getInput('repository', { required: false });
+    let tag = core.getInput('tag', { required: false });
+
+    if (accessKeyId.length > 0 && accessKeySecret.length > 0) {
+        if (regionId.length == 0) {
+            core.setFailed(`Action failed for region-id is missing`);
+        }
+        
+        endpoint = getAPIEndpoint(regionId);
+
+        if (instanceId.length == 0) {
+
+            try {
+                var client = new ROAClient({
+                    accessKeyId,
+                    accessKeySecret,
+                    endpoint: endpoint,
+                    apiVersion: '2016-06-07'
+                });
+                let url = `/repos/${repository}/tags/${tag}/scan`
+                console.log(`Scanning image ${repository}/${tag} with url ${url} ...`);
+
+                await client.request('PUT', url)
+                console.log(`Scanning image ${repository}/${tag} ...`);
+            } catch (err) {
+                core.setFailed(`Action failed to scan image with error: ${err}`);
+            }
+        } else {
+            var client = new RPCClient({
+                accessKeyId,
+                accessKeySecret,
+                endpoint: endpoint,
+                codes: ['success'], 
+                apiVersion: '2018-12-01'
+            });
+            let repoId = ""
+            try {
+                let repo = repository.split("/",2)
+                let repoResp = await client.request("GetRepository", {
+                    InstanceId: instanceId,
+                    RegionId: regionId,
+                    RepoStatus: 'NORMAL',
+                    RepoName: repo[1],
+                    RepoNamespaceName: repo[0]
+                });
+                repoId = repoResp.RepoId
+                console.log(`Found image repository ${repository} with repoId ${repoId}`);
+            } catch (err) {
+                core.setFailed(`Action failed to find image repository ${repository} with error: ${err}`);
+            }
+            try {
+                let result = await client.request("CreateRepoTagScanTask", {
+                    InstanceId: instanceId,
+                    RegionId: regionId,
+                    RepoId: repoId,
+                    Tag: tag
+                });
+                let requestId = result.RequestId
+                console.log(`Scanning image ${repository}/${tag} with requestId ${requestId} ...`);
+            } catch (err) {
+                core.setFailed(`Action failed to scan image ${repository}/${tag} with error: ${err}`);
+            }
+        }
+    } else {
+        core.setFailed(`access-key-id or access-key-secret is missing`);
+    }
+}
+
+run().catch(core.setFailed);
